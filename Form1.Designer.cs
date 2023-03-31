91f238ce3f7d967a56e3463bd02d4c2eedfd291c
Kenan Zudjelovic <kzudjelovic@gmail.com>
2023-03-31 21:16:32 +0200

    First mini app.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,189 @@ namespace app1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbComment = new System.Windows.Forms.RichTextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tbPreview = new System.Windows.Forms.RichTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(87, 103);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(57, 25);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "User";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(87, 183);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(106, 25);
+            this.Label2.TabIndex = 1;
+            this.Label2.Text = "Password";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(87, 316);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(103, 25);
+            this.Label3.TabIndex = 2;
+            this.Label3.Text = "Comment";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(87, 545);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(88, 25);
+            this.Label4.TabIndex = 3;
+            this.Label4.Text = "Preview";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(274, 103);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(282, 31);
+            this.tbUser.TabIndex = 4;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(274, 180);
+            this.tbPass.MaxLength = 6;
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(282, 31);
+            this.tbPass.TabIndex = 5;
+            // 
+            // tbComment
+            // 
+            this.tbComment.Location = new System.Drawing.Point(274, 313);
+            this.tbComment.Name = "tbComment";
+            this.tbComment.Size = new System.Drawing.Size(335, 184);
+            this.tbComment.TabIndex = 6;
+            this.tbComment.Text = "";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfirm.Location = new System.Drawing.Point(780, 93);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(111, 41);
+            this.btnConfirm.TabIndex = 7;
+            this.btnConfirm.Text = "&Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnOdustani
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Location = new System.Drawing.Point(780, 171);
+            this.btnReset.Name = "btnOdustani";
+            this.btnReset.Size = new System.Drawing.Size(120, 40);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnZatvori
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Location = new System.Drawing.Point(780, 357);
+            this.btnClose.Name = "btnZatvori";
+            this.btnClose.Size = new System.Drawing.Size(120, 38);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tbPreview
+            // 
+            this.tbPreview.Location = new System.Drawing.Point(221, 545);
+            this.tbPreview.Name = "tbPreview";
+            this.tbPreview.ReadOnly = true;
+            this.tbPreview.Size = new System.Drawing.Size(519, 184);
+            this.tbPreview.TabIndex = 10;
+            this.tbPreview.Text = "";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(798, 525);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(98, 25);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Belgrade";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(828, 553);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 244);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1165, 826);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.tbPreview);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.tbComment);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.RichTextBox tbComment;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox tbPreview;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,29 @@ namespace app1
         {
             InitializeComponent();
         }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            this.tbPreview.Text = "Username: " + this.tbUser.Text + "\r\n\r\nComment: " + tbComment.Text;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tbUser.Clear();
+            tbPass.Clear();
+            tbComment.Text = "";
+            tbPreview.Text = "";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.beograd.rs");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
